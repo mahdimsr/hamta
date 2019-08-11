1c30f3eb6d81a2ab196d8b61dffb53e667c4a6d3
mahdimsr <mahdi.msr4@gmail.com>
2019-08-11 15:18:42 +0430

    Update LoginController.php

--- app/Http/Controllers/Student/Auth/LoginController.php
+++ app/Http/Controllers/Student/Auth/LoginController.php
@@ -15,47 +15,61 @@ class LoginController extends Controller
 	public function show()
 	{
 		return view('student.auth.login');
-    }
+	}
+
+
 
 	public function login(Request $request)
 	{
-        $this->validate($request,
-        [
-            'mobile-email'=>'required',
-            'password'=>'required'
-        ],
-        [
-            'mobile-email.required'=>'شماره تلفن همراه یا ایمیل خود را وارد نمایید.',
-            'password.required'=>'کلمه عبور خود را وارد نمایید.',
-        ]
-        );
-		if (Auth::attempt(['mobile' =>$request->input('mobile-email') , 'password' =>$request->input('password')], $request->input('remember')))
+		$this->validate($request,
+			[
+				'mobile-email' => 'required',
+				'password'     => 'required',
+			],
+			[
+				'mobile-email.required' => 'شماره تلفن همراه یا ایمیل خود را وارد نمایید.',
+				'password.required'     => 'کلمه عبور خود را وارد نمایید.',
+			]
+		);
+		if (Auth::attempt(['mobile'   => $request->input('mobile-email'),
+						   'password' => $request->input('password'),
+		], $request->input('remember')))
 		{
-            if ($request->has('remember'))
-            {
-                Cookie::queue('studentuser', $request->input('mobile-email') , 6*24*60 );
-                Cookie::queue('studentpass', $request->input('password') , 6*24*60 );
-            }
-            else
-            {
-                Cookie::queue('studentuser','');
-                Cookie::queue('studentpass','');
-            }
-		}
-		else if(Auth::attempt(['email'=>$request->input('mobile-email') , 'password' =>$request->input('password')], $request->input('remember')))
-		{
-            if ($request->has('remember'))
-            {
-                Cookie::queue('studentuser', $request->input('mobile-email') , 6*24*60 );
-                Cookie::queue('studentpass', $request->input('password') , 6*24*60 );
-            }
-            else
-            {
-                Cookie::queue('studentuser','');
-                Cookie::queue('studentpass','');
+			if ($request->has('remember'))
+			{
+				Cookie::queue('studentuser', $request->input('mobile-email'), 6 * 24 * 60);
+				Cookie::queue('studentpass', $request->input('password'), 6 * 24 * 60);
+			}
+			else
+			{
+				Cookie::queue('studentuser', '');
+				Cookie::queue('studentpass', '');
+			}
 
-            }
-        }
-    }
+			return redirect()->route('admin_dashboard');
+		}
+		else if (Auth::attempt(['email'    => $request->input('mobile-email'),
+								'password' => $request->input('password'),
+		], $request->input('remember')))
+		{
+			if ($request->has('remember'))
+			{
+				Cookie::queue('studentuser', $request->input('mobile-email'), 6 * 24 * 60);
+				Cookie::queue('studentpass', $request->input('password'), 6 * 24 * 60);
+			}
+			else
+			{
+				Cookie::queue('studentuser', '');
+				Cookie::queue('studentpass', '');
+
+			}
+
+			return redirect()->route('admin_dashboard');
+		}
+		else
+		{
+			return redirect()->route('student_login_show');
+		}
+	}
 
 }
