b40997c0fb7ccb212f9118f174fa1fef145d3491
mahdimsr <mahdi.msr4@gmail.com>
2019-08-13 13:47:31 +0430

    gradeLesson seeder  and relations

--- app/model/GradeLesson.php
+++ app/model/GradeLesson.php
@@ -10,11 +10,39 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  * @property \Carbon\Carbon $created_at
  * @property \Carbon\Carbon $updated_at
  * @property int $id
+ * @property int $lessonId
+ * @property int $gradeId
+ * @property int $orientationId
+ * @property int $ratio
+ * @property string $type
  * @property \Carbon\Carbon $deleted_at
  */
 class GradeLesson extends Model
 {
-    use SoftDeletes;
+	use SoftDeletes;
 
-    protected $table = 'grade_lesson';
+	protected $table = 'grade_lesson';
+
+
+
+	public function orientation()
+	{
+		return $this->belongsTo(Orientation::class,'orientationsId');
+	}
+
+
+
+	public function grade()
+	{
+		return $this->belongsTo(Grade::class,'gradeId');
+	}
+
+
+
+	public function lesson()
+	{
+		return $this->belongsTo(Lesson::class,'lessonId');
+	}
+
+
 }
