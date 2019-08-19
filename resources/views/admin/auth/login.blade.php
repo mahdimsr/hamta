3be4c0ac5ff9be91da16cf09700c75e45f7bb929
mahdimsr <mahdi.msr4@gmail.com>
2019-08-19 16:49:46 +0430

    Update login.blade.php

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+	<meta charset='utf-8'>
+	<meta http-equiv='X-UA-Compatible' content='IE=edge'>
 	<title>ورود ادمین</title>
 	<link rel='stylesheet' type='text/css' media='screen' href="{{asset('fonts/font.css')}}">
 
-    <link rel='stylesheet' type='text/css' media='screen' href="{{asset('css/admin/auth/style.css')}}">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-   
+	<link rel='stylesheet' type='text/css' media='screen' href="{{asset('css/admin/auth/style.css')}}">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+		  integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
 </head>
 <body>
 <form method="post" action="{{ route('admin_login_submit')}}">
@@ -19,13 +20,13 @@
 
 	<div class="loginbox">
 		<img src="{{asset('image/admin/auth/man.png')}}" class="user">
-		<h3>ورود ادمین</h3>
+		<h3>ورود</h3>
 
 		<div class="inputbox">
 			<input type="text" name="username" placeholder="نام کاربری"
 				   value="{{ $adminInfo ? $adminInfo : old('username')}}">
 			<span><i class="fas fa-users" aria-hidden="true"></i></span>
-			<small>{{  $errors->first('username')}}</small>
+			<div class="has-error">{{  $errors->first('username')}}</div>
 
 		</div>
 
@@ -33,9 +34,9 @@
 			<input type="text" name="password" placeholder="رمز عبور"
 				   value="{{ $adminPass ? $adminPass : old('password') }}">
 			<span><i class="fas fa-lock"></i></span>
-			<small>{{$errors->first('password')}}</small>
+			<div class="has-error">{{$errors->first('password')}}</div>
 		</div>
-		
+
 
 		<div class="con">
 			<label>مرا به خاطر داشته باش
@@ -45,20 +46,11 @@
 		</div>
 
 		@if($errors->any())
-			<small class="err">{{$errors->first('message')}}</small>
+			<div class="has-error err">{{$errors->first('message')}}</div>
 		@endif
 		<input type="submit" value="ورود">
 	</div>
 
-				<div class="con">
-              <label>مرا به خاطر داشته باش
-                  <input type="checkbox" name="remember">
-                  <span class="checkmark"></span>
-                </label>
-				</div>
-				<p class="err">خطا در اتصال</p>
-
-   </div>
 </form>
 
 </body>
