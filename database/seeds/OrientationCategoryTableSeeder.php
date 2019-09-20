1e1ea82c34c80314567d44d9194a59a738396430
Alireza Rabiee <53295065+apaliray03@users.noreply.github.com>
2019-09-20 19:15:16 +0430

    Update OrientationCategoryTableSeeder.php

--- database/seeds/OrientationCategoryTableSeeder.php
+++ database/seeds/OrientationCategoryTableSeeder.php
@@ -12,17 +12,20 @@ class OrientationCategoryTableSeeder extends Seeder
      */
     public function run()
     {
+
+        //MathematicPhysics
+
 		$OrientationCategory                  = new OrientationCategory();
 		$OrientationCategory->orientationId   = '1';
 		$OrientationCategory->categoryId      = '1';
-        $OrientationCategory->ratio           = '2';
+        $OrientationCategory->ratio           = '4';
         $OrientationCategory->type            = 'GENERAL';
         $OrientationCategory->save();
 
         $OrientationCategory                  = new OrientationCategory();
 		$OrientationCategory->orientationId   = '1';
 		$OrientationCategory->categoryId      = '2';
-        $OrientationCategory->ratio           = '2';
+        $OrientationCategory->ratio           = '3';
         $OrientationCategory->type            = 'GENERAL';
         $OrientationCategory->save();
 
@@ -57,21 +60,23 @@ class OrientationCategoryTableSeeder extends Seeder
         $OrientationCategory                  = new OrientationCategory();
 		$OrientationCategory->orientationId   = '1';
 		$OrientationCategory->categoryId      = '7';
-        $OrientationCategory->ratio           = '3';
+        $OrientationCategory->ratio           = '2';
         $OrientationCategory->type            = 'EXPERT';
         $OrientationCategory->save();
+
+        //Science
 
         $OrientationCategory                  = new OrientationCategory();
 		$OrientationCategory->orientationId   = '2';
 		$OrientationCategory->categoryId      = '1';
-        $OrientationCategory->ratio           = '2';
+        $OrientationCategory->ratio           = '4';
         $OrientationCategory->type            = 'GENERAL';
         $OrientationCategory->save();
 
         $OrientationCategory                  = new OrientationCategory();
 		$OrientationCategory->orientationId   = '2';
 		$OrientationCategory->categoryId      = '2';
-        $OrientationCategory->ratio           = '2';
+        $OrientationCategory->ratio           = '3';
         $OrientationCategory->type            = 'GENERAL';
         $OrientationCategory->save();
 
@@ -92,14 +97,14 @@ class OrientationCategoryTableSeeder extends Seeder
         $OrientationCategory                  = new OrientationCategory();
 		$OrientationCategory->orientationId   = '2';
 		$OrientationCategory->categoryId      = '5';
-        $OrientationCategory->ratio           = '3';
+        $OrientationCategory->ratio           = '2';
         $OrientationCategory->type            = 'EXPERT';
         $OrientationCategory->save();
 
         $OrientationCategory                  = new OrientationCategory();
 		$OrientationCategory->orientationId   = '2';
 		$OrientationCategory->categoryId      = '6';
-        $OrientationCategory->ratio           = '3';
+        $OrientationCategory->ratio           = '2';
         $OrientationCategory->type            = 'EXPERT';
         $OrientationCategory->save();
 
@@ -124,17 +129,19 @@ class OrientationCategoryTableSeeder extends Seeder
         $OrientationCategory->type            = 'EXPERT';
         $OrientationCategory->save();
 
+        //Literature and Humanities
+
         $OrientationCategory                  = new OrientationCategory();
 		$OrientationCategory->orientationId   = '3';
 		$OrientationCategory->categoryId      = '1';
-        $OrientationCategory->ratio           = '2';
+        $OrientationCategory->ratio           = '4';
         $OrientationCategory->type            = 'GENERAL';
         $OrientationCategory->save();
 
         $OrientationCategory                  = new OrientationCategory();
 		$OrientationCategory->orientationId   = '3';
 		$OrientationCategory->categoryId      = '2';
-        $OrientationCategory->ratio           = '2';
+        $OrientationCategory->ratio           = '3';
         $OrientationCategory->type            = 'GENERAL';
         $OrientationCategory->save();
 
@@ -155,49 +162,49 @@ class OrientationCategoryTableSeeder extends Seeder
         $OrientationCategory                  = new OrientationCategory();
 		$OrientationCategory->orientationId   = '3';
 		$OrientationCategory->categoryId      = '5';
-        $OrientationCategory->ratio           = '4';
+        $OrientationCategory->ratio           = '2';
         $OrientationCategory->type            = 'EXPERT';
         $OrientationCategory->save();
 
         $OrientationCategory                  = new OrientationCategory();
 		$OrientationCategory->orientationId   = '3';
 		$OrientationCategory->categoryId      = '10';
-        $OrientationCategory->ratio           = '3';
+        $OrientationCategory->ratio           = '1';
         $OrientationCategory->type            = 'EXPERT';
         $OrientationCategory->save();
 
         $OrientationCategory                  = new OrientationCategory();
 		$OrientationCategory->orientationId   = '3';
 		$OrientationCategory->categoryId      = '1';
-        $OrientationCategory->ratio           = '3';
+        $OrientationCategory->ratio           = '4';
         $OrientationCategory->type            = 'EXPERT';
         $OrientationCategory->save();
 
         $OrientationCategory                  = new OrientationCategory();
 		$OrientationCategory->orientationId   = '3';
 		$OrientationCategory->categoryId      = '2';
-        $OrientationCategory->ratio           = '3';
+        $OrientationCategory->ratio           = '4';
         $OrientationCategory->type            = 'EXPERT';
         $OrientationCategory->save();
 
         $OrientationCategory                  = new OrientationCategory();
 		$OrientationCategory->orientationId   = '3';
 		$OrientationCategory->categoryId      = '11';
-        $OrientationCategory->ratio           = '3';
+        $OrientationCategory->ratio           = '1';
         $OrientationCategory->type            = 'EXPERT';
         $OrientationCategory->save();
 
         $OrientationCategory                  = new OrientationCategory();
 		$OrientationCategory->orientationId   = '3';
 		$OrientationCategory->categoryId      = '12';
-        $OrientationCategory->ratio           = '3';
+        $OrientationCategory->ratio           = '1';
         $OrientationCategory->type            = 'EXPERT';
         $OrientationCategory->save();
 
         $OrientationCategory                  = new OrientationCategory();
 		$OrientationCategory->orientationId   = '3';
 		$OrientationCategory->categoryId      = '13';
-        $OrientationCategory->ratio           = '3';
+        $OrientationCategory->ratio           = '1';
         $OrientationCategory->type            = 'EXPERT';
         $OrientationCategory->save();
 
@@ -218,7 +225,7 @@ class OrientationCategoryTableSeeder extends Seeder
         $OrientationCategory                  = new OrientationCategory();
 		$OrientationCategory->orientationId   = '3';
 		$OrientationCategory->categoryId      = '16';
-        $OrientationCategory->ratio           = '3';
+        $OrientationCategory->ratio           = '1';
         $OrientationCategory->type            = 'EXPERT';
         $OrientationCategory->save();
     }
