2b346942dcff945fd8b70516366c379d6535051e
price76 <39560681+price76@users.noreply.github.com>
2019-10-12 00:37:41 +0330

    modal

--- resources/views/student/dashboard/lessonExam/exam_questions.blade.php
+++ resources/views/student/dashboard/lessonExam/exam_questions.blade.php
@@ -5,60 +5,26 @@
 
 
 @section('content')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
 
 <link rel="stylesheet" href="{{ asset('css/student/dashboard/exam-questions.css') }}">
 
 
-<div dir="rtl" class="row">
-    <div class="col-md-1">
-        <label class="label" style="font-size: 130%">
-            <input name="questionId[]" value="" id="question-check" onchange="sina()"
-
-                   class="label__checkbox" type="checkbox"/>
-            <span class="label__text">
-                  <span class="label__check">
-                        <i class="fa fa-check icon"></i>
-                </span>
-            </span>
-        </label>
-    </div>
-
-    <div class="col-md-11">
-        <div class="card cards">
-
-                <div class="card-header" id="correct-item1">این سوال است ببینید این سوال است ببینید این سوال است ببینید</div>
-
-            <div class="card-body">
-
-                <div dir="rtl" class="row text-right " style="margin: 0px;">
-                    <div class="col-md-9 col-lg-10" style="float: right">
 
 
-                        <ul class="answers">
-                            <li class="list-li"><p>گزینه است </p></li>
-                            <li class="list-li"><p> گزینه است </p></li>
-                            <li class="list-li"><p>گزینه است </p></li>
-                            <li class="list-li"><p>گزینه است </p></li>
-                        </ul>
-
-                    </div>
-                    <div class="col-md-3 col-lg-2 col-sm-2" style="float: right;">
-                        <a href="#"> <img class="question-img  imgs"
-                                          src="{{asset('image/admin/dashboard/test.png')}}"
-                                          width="120px" height="100px"></a>
-                    </div>
 
 
-                </div>
 
-            </div>
-            <div class="card-footer text-muted text-center" id="correct-item2">
-                <p>مقطع: سوم ، سطح سختی: آسان</p>
-            </div>
-        </div>
-    </div>
 
-</div>
+
+
+
+
+
+
+
+
+
 <div class="row">
         <div class="col-12">
             <h3>این قسمت مربوط به توضیحات ازمون می باشد</h3>
@@ -165,9 +131,11 @@
 <p>ایا رسیدن به سرعت مجاز است؟ -1</p>
 
 
-
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="{{asset('image/homepage/city_of_love-wallpaper-3554x1999.jpg')}}" alt="Card image cap">
+<div class="gallery">
+  <figure>
+    <img src="{{asset('image/homepage/city_of_love-wallpaper-3554x1999.jpg')}}" alt="" />
+ 
+  </figure>
 
 </div>
 </li>
@@ -236,6 +204,7 @@
 
 </li>
 </ul>
+
 
 
 
