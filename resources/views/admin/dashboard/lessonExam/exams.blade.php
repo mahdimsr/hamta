bd300c09706c1b231adad22fe21eac686a6b285b
price76 <39560681+price76@users.noreply.github.com>
2019-10-13 20:16:31 +0330

    admin modal

--- resources/views/admin/dashboard/lessonExam/exams.blade.php
+++ resources/views/admin/dashboard/lessonExam/exams.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.admin_dashboard')
 
 @section('content')
-
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+<link rel="stylesheet" href="{{ asset('css/admin/dashboard/popup.css') }}">
 	<div class="row">
 		<div class="col-md-12">
 			<div class="card">
@@ -33,15 +34,46 @@
                                 <td>{{count($exam->questionExams)}}</td>
 								<td>
 
-									<a href="{{route('admin_ltlExams_remove',['exm' => $exam->exm])}}" id="remove-btn"
-									   type="button"
-									   style="font-size: 12px;" class="btn btn-danger">
-										حذف
-									</a>
+										{{-- <a href="{{route('admin_ltlExams_remove',['exm' => $exam->exm])}}" id="remove-btn"
+												type="button"
+												style="font-size: 12px;" class="btn btn-danger">
+												 حذف
+											 </a>
+		                                        --}}
+
+
+
+
+										<button data-modal-trigger="trigger-1" class="trigger btn btn-danger">
+												
+												حذف
+											</button>
+											
+											
+											<div data-modal="trigger-1" class="modal">
+												<article class="content-wrapper">
+													<button class="close"></button>
+													<header class="modal-header">
+														<h2>حذف اطلاعات</h2>
+													</header>
+													<div class="content">
+														<p>ایا مایل هستید آزمون درس به درس ایحاد شده حذف شود؟</p>
+													</div>
+													<footer class="modal-footer">
+														<button class="action">حذف شود</button>
+														<button class="action">حذف نشود</button>
+													</footer>
+												</article>
+											</div>
+
+
 									<a href="{{route('admin_ltlExams_editShow',['exm' => $exam->exm])}}"
 									   style="font-size: 12px;" class="btn btn-info">
 										ویرایش
 									</a>
+
+
+
 
 									<a href="{{route('admin_ltlExams_questionsShow',['exm' => $exam->exm])}}"
 									   style="font-size: 12px;" class="btn btn-success">
@@ -64,7 +96,26 @@
 @section('script')
 
 	<script>
+		const buttons = document.querySelectorAll(`button[data-modal-trigger]`);
 
+for(let button of buttons) {
+	modalEvent(button);
+}
+
+function modalEvent(button) {
+	button.addEventListener('click', () => {
+		const trigger = button.getAttribute('data-modal-trigger');
+		const modal = document.querySelector(`[data-modal=${trigger}]`);
+		const contentWrapper = modal.querySelector('.content-wrapper');
+		const close = modal.querySelector('.close');
+
+		close.addEventListener('click', () => modal.classList.remove('open'));
+		modal.addEventListener('click', () => modal.classList.remove('open'));
+		contentWrapper.addEventListener('click', (e) => e.stopPropagation());
+
+		modal.classList.toggle('open');
+	});
+}
 	</script>
 
 @endsection
