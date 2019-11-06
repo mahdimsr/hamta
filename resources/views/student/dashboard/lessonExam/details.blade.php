ef4368286d0ce41ab921831de3f8e56a58ea0b04
price76 <39560681+price76@users.noreply.github.com>
2019-11-06 20:04:51 +0330

    think

--- resources/views/student/dashboard/lessonExam/details.blade.php
+++ resources/views/student/dashboard/lessonExam/details.blade.php
@@ -6,7 +6,7 @@
 
 
 
-<div class="back-bg" style="background-image:url(../../../image/student/auth/auth.jpg)">
+<div class="back-bg" style="background-image:url({{asset('image/student/auth/auth.jpg')}})">
 
 <div class="row"><h3 class="exam-title" dir="rtl">ازمون شماره 1</h3></div>
     <div class="container">
@@ -87,9 +87,15 @@
 @section('script')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TimelineMax.min.js"></script>
+
 <script >
 const back_bg= document.querySelector(".back-bg");
 const jumbotron= document.querySelector(".jumbotron");
+const description_body= document.querySelector(".description-body");
+
+
+
+
 const tl = new TimelineMax();
 tl.fromTo(back_bg,1,{height: "0%"},{height: "80%" , ease: Power2.easeInOut})
 .fromTo(
@@ -99,6 +105,10 @@ tl.fromTo(back_bg,1,{height: "0%"},{height: "80%" , ease: Power2.easeInOut})
     {width:"96%" , ease: Power2.easeInOut}
 )
 .fromTo(jumbotron, 1, {opacity:0},{opacity: 1},"-=0.7")
+
+
+
+
 </script>
 @endsection
 
