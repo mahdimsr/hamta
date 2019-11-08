8a90b9c693a686285b550888b9df28b9ea7d1d93
sinadl <45151701+sinadl@users.noreply.github.com>
2019-11-09 02:47:01 +0330

    dashboard

--- resources/views/layouts/student_dashboard.blade.php
+++ resources/views/layouts/student_dashboard.blade.php
@@ -110,33 +110,53 @@
 
 <hr id="sidebar-hr">
 
-                <li class="{{Illuminate\Support\Facades\Route::currentRouteName() == 'student_dashboard_scholarship' ? 'active' : ''}}">
-                    <a href="#">
+                <li class="{{Illuminate\Support\Facades\Route::currentRouteName() == 'student_dashboard_scholarship' ? 'active' : ''}} disabled">
+                    <a >
                         <i class="fa fa-graduation-cap"></i>
-                        <p class="slider-par">کدهای تخفیف من</p>
-                        <span class="slider-span">تعداد کدهای تخفیف قابل استفاده:</span>
+                        <p class="slider-par">فرصتی تا کنکور</p>
+                        <span class="slider-span">نکات کنکوری</span>
                     </a>
                 </li>
-
-<hr id="sidebar-hr">
-                <li style="margin-bottom: 8px;">
-                    <a>
-                    <p class="slider-par">:باقیمانده کیف پول</p>
-                    </a>
-                    <a id="addfund1" href="#">
-                         <span id="addfund">< خرید مقداری اعتبار</span>
-                    </a>
-                </li>
-
-
-            </ul>
 
  
             <hr id="sidebar-hr">
+            <li class="{{Illuminate\Support\Facades\Route::currentRouteName() == 'student_dashboard_scholarship' ? 'active' : ''}} disabled">
+                <a >
+                    <i class="fa fa-graduation-cap"></i>
+                    <p class="slider-par">بازی و سرگرمی</p>
+                    <span class="slider-span">:تعداد سرگرمی ها</span>
+                </a>
+            </li>
 
+            <hr id="sidebar-hr">
+            <li class="{{Illuminate\Support\Facades\Route::currentRouteName() == 'student_dashboard_scholarship' ? 'active' : ''}} disabled">
+                <a>
+                    <i class="fa fa-graduation-cap"></i>
+                    <p class="slider-par">معرفی دبیر</p>
+                    <span class="slider-span">:تعداد دبیران معرفی شده</span>
+                </a>
+            </li>
 
+            <hr id="sidebar-hr">
+            <li class="{{Illuminate\Support\Facades\Route::currentRouteName() == 'student_dashboard_scholarship' ? 'active' : ''}} disabled">
+                <a >
+                    <i class="fa fa-graduation-cap"></i>
+                    <p class="slider-par">بحث و گفتگو</p>
+                    <span class="slider-span">:تعداد افراد آنلاین</span>
+                </a>
+            </li>
 
+                <hr id="sidebar-hr">
+                <li style="margin-bottom: 8px;">
+                    <a>
+                        <p class="slider-par">:باقیمانده کیف پول</p>
+                    </a>
+                    <a id="addfund1" href="#">
+                        <span id="addfund">< خرید مقداری اعتبار</span>
+                    </a>
+                </li>
 
+            </ul>
         </div>
     </div>
 
