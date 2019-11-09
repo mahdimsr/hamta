94147be5ccf512c55371d2ac787d753738e04e81
Alireza Rabiee <53295065+apaliray03@users.noreply.github.com>
2019-11-10 00:40:42 +0330

    Update transactions.blade.php

--- resources/views/student/dashboard/transaction/transactions.blade.php
+++ resources/views/student/dashboard/transaction/transactions.blade.php
@@ -4,55 +4,56 @@
 
 @endsection
 @section('content')
+@if($purchases->isEmpty())
+<div id="app">
+    <div class="when-empty">
+        <h4 dir="rtl">تراکنشی وجود ندارد</h4>
+    <img class="when-empty-image" src="{{ asset('image/student/dashboard/empty_cart.svg') }}">
 
+    </div>
+    </div>
+@else
 <section class="pink-bg row" >
 
-<img class="fisrt-jpg" src="{{ asset('image/student/dashboard/Group 180.png') }}" alt="">
+    <img class="fisrt-jpg" src="{{ asset('image/student/dashboard/Group 180.png') }}" alt="">
 
-</section>
-<section class="factor row">
-<h4 dir="rtl">فاکتورها</h4>
+    </section>
+    <section class="factor row">
+    <h4 dir="rtl">فاکتورها</h4>
 
-<div id="app">
-    @foreach($purchases as $purchase)
-<div class="card" dir="rtl">
-        <div class="card-body">
-
-
-          <p class="card-body-item">نوع پرداخت :</p>
-          <p class="card-body-item-second">خرید آزمون درس به درس</p>
-
-        <div>
-            <p class="card-body-item">کد فاکتور :</p>
-            <p class="card-body-item-second">{{ $purchase->code }}</p>
-        </div>
-
-          <div class="card-body">
-              <p class="card-body-item">قیمت :</p>
-              <p class="card-body-item-second">{{ $purchase->price }}</p>
-        </div>
-
-            @if($purchase->discountPrice )
+    <div id="app">
+        @foreach($purchases as $purchase)
+    <div class="card" dir="rtl">
             <div class="card-body">
-                <p class="card-body-item">قیمت با کد تخفیف :</p>
-                <p class="card-body-item-with">{{ $purchase->discountPrice }}</p>
-                <p class="card-body-item-second">{{ $purchase->discount->code }}</p>
+
+
+              <p class="card-body-item">نوع پرداخت :</p>
+              <p class="card-body-item-second">خرید آزمون درس به درس</p>
+
+            <div>
+                <p class="card-body-item">کد فاکتور :</p>
+                <p class="card-body-item-second">{{ $purchase->code }}</p>
             </div>
-            @endif
 
-        </div>
-      </div>
-@endforeach
-</div>
+              <div class="card-body">
+                  <p class="card-body-item">قیمت :</p>
+                  <p class="card-body-item-second">{{ number_format($purchase->price) }} تومان</p>
+            </div>
 
-<div id="app">
-<div class="when-empty">
-    <h4 dir="rtl">تراکنشی وجود ندارد</h4>
-<img class="when-empty-image" src="{{ asset('image/student/dashboard/empty_cart.svg') }}">
+                @if($purchase->discountPrice )
+                <div class="card-body">
+                    <p class="card-body-item">قیمت با کد تخفیف :</p>
+                    <p class="card-body-item-with">{{ $purchase->discount->code }}</p>
+                    <p class="card-body-item-second">{{ number_format($purchase->discountPrice) }} تومان</p>
+                </div>
+                @endif
 
-</div>
-</div>
-
+            </div>
+          </div>
+    @endforeach
+    </div>
+    </section>
+@endif
 
 @endsection
 @section('script')
