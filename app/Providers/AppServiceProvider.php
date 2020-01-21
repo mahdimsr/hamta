cdcc78bbeef5fa2cf74d90b8c799527473e912fd
Alireza Rabiee <53295065+apaliray03@users.noreply.github.com>
2020-01-21 15:48:06 +0330

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -34,27 +34,26 @@ use Illuminate\Support\ServiceProvider;
 
             view()->composer('*', function($view)
             {
-                $authId             = Auth::guard('student')->id();
-                $discountNumbers    = 0;
-
-                $cart           = Cart::query()->where('studentId', $authId)->where('transactionId',0)->get();
-                $purchasedExams = Transaction::query()->where('studentId',$authId)->where('type','PURCHASE')->where('itemType','LESSON_EXAM')->where('status','SUCCESS')->get();
-                $studentCodes   = StudentCode::query()->where('studentId',$authId)->get();
-                $scholarship    = Scholarship::query()->where('studentId',$authId)->first();
-
-                foreach($studentCodes as $studentCode)
-                {
-                    if(!$studentCode->discount->isExpired)
-                        $discountNumbers++;
-                }
-
-                $view->with('cart', $cart);
-                $view->with('discountNumbers', $discountNumbers);
-                $view->with('scholarship', $scholarship);
-                $view->with('examNumbers', count($purchasedExams));
 
                 if (Auth::guard('student')->check())
                 {
+                    $authId             = Auth::guard('student')->id();
+                    $discountNumbers    = 0;
+                    $cart               = Cart::query()->where('studentId', $authId)->where('transactionId',0)->get();
+                    $purchasedExams     = Transaction::query()->where('studentId',$authId)->where('type','PURCHASE')->where('itemType','LESSON_EXAM')->where('status','SUCCESS')->get();
+                    $studentCodes       = StudentCode::query()->where('studentId',$authId)->get();
+                    $scholarship        = Scholarship::query()->where('studentId',$authId)->first();
+
+                    foreach($studentCodes as $studentCode)
+                    {
+                        if(!$studentCode->discount->isExpired)
+                            $discountNumbers++;
+                    }
+
+                    $view->with('cart', $cart);
+                    $view->with('discountNumbers', $discountNumbers);
+                    $view->with('scholarship', $scholarship);
+                    $view->with('examNumbers', count($purchasedExams));
                     $view->with('student', Auth::guard('student')->user());
                 }
 
